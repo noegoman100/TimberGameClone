8ca04df1d8ca0836179664d0185a9f539bb92520
noegoman100 <38665607+noegoman100@users.noreply.github.com>
2020-08-11 22:44:37 -0600

    Working. Refactoring Code (Clouds)

--- Timber.cpp
+++ Timber.cpp
@@ -64,29 +64,7 @@ int main()
 		spriteCloud[i].setTexture(textureCloud);
 		spriteCloud[i].setPosition(0, i * 250);
 	}
-	/*spriteCloud[0].setTexture(textureCloud);
-	spriteCloud[1].setTexture(textureCloud);
-	spriteCloud[2].setTexture(textureCloud);
-	spriteCloud[0].setPosition(0, 0);
-	spriteCloud[1].setPosition(0, 250);
-	spriteCloud[2].setPosition(0, 500);*/
-	////TODO remove below - clouds. 
-	//Sprite spriteCloud1(textureCloud);
-	//Sprite spriteCloud2(textureCloud);
-	//Sprite spriteCloud3(textureCloud);
-	//// Position the clouds on the left of the screen
-	//// at different heights
-	//spriteCloud1.setPosition(0, 0);
-	//spriteCloud2.setPosition(0, 250);
-	//spriteCloud3.setPosition(0, 500);
-	//// Are the clouds currently on screen?
-	//bool cloud1Active{ false };
-	//bool cloud2Active{ false };
-	//bool cloud3Active{ false };
-	//// How fast is each cloud?
-	//float cloud1Speed{ 0.0f };
-	//float cloud2Speed{ 0.0f };
-	//float cloud3Speed{ 0.0f };
+
 
 	// Variables to control time itself
 	Clock clock;
@@ -355,83 +333,7 @@ int main()
 				}
 			}
 
-			//// Cloud 1
-			//if (!cloud1Active)
-			//{
-			//	// How fast is the cloud
-			//	srand((int)time(0) * 10);
-			//	cloud1Speed = (rand() % 200);
-			//	// How high is the cloud
-			//	srand((int)time(0) * 10);
-			//	float height = (rand() % 150);
-			//	spriteCloud1.setPosition(-200, height);
-			//	cloud1Active = true;
-			//}
-			//else
-			//{
-			//	spriteCloud1.setPosition(
-			//		spriteCloud1.getPosition().x +
-			//		(cloud1Speed * dt.asSeconds()),
-			//		spriteCloud1.getPosition().y);
-			//	// Has the cloud reached the right hand edge of the screen?
-			//	if (spriteCloud1.getPosition().x > 1920)
-			//	{
-			//		// Set it up ready to be a whole new cloud next frame
-			//		cloud1Active = false;
-			//	}
-			//}
-
-			//// Cloud 2
-			//if (!cloud2Active)
-			//{
-			//	// How fast is the cloud
-			//	srand((int)time(0) * 20);
-			//	cloud2Speed = (rand() % 200);
-			//	// How high is the cloud
-			//	srand((int)time(0) * 20);
-			//	float height = (rand() % 300) - 150;
-			//	spriteCloud2.setPosition(-200, height);
-			//	cloud2Active = true;
-			//}
-			//else
-			//{
-			//	spriteCloud2.setPosition(
-			//		spriteCloud2.getPosition().x +
-			//		(cloud2Speed * dt.asSeconds()),
-			//		spriteCloud2.getPosition().y);
-			//	// Has the cloud reached the right hand edge of the screen?
-			//	if (spriteCloud2.getPosition().x > 1920)
-			//	{
-			//		// Set it up ready to be a whole new cloud next frame
-			//		cloud2Active = false;
-			//	}
-			//}
-
-			//// Cloud 3
-			//if (!cloud3Active)
-			//{
-			//	// How fast is the cloud
-			//	srand((int)time(0) * 30);
-			//	cloud3Speed = (rand() % 200);
-			//	// How high is the cloud
-			//	srand((int)time(0) * 30);
-			//	float height = (rand() % 450) - 150;
-			//	spriteCloud3.setPosition(-200, height);
-			//	cloud3Active = true;
-			//}
-			//else
-			//{
-			//	spriteCloud3.setPosition(
-			//		spriteCloud3.getPosition().x +
-			//		(cloud3Speed * dt.asSeconds()),
-			//		spriteCloud3.getPosition().y);
-			//	// Has the cloud reached the right hand edge of the screen?
-			//	if (spriteCloud3.getPosition().x > 1920)
-			//	{
-			//		// Set it up ready to be a whole new cloud next frame
-			//		cloud3Active = false;
-			//	}
-			//}
+			
 
 			// Update the score text
 			std::stringstream ss;
